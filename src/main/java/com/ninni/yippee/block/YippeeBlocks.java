daa31e5ffde86f5e58d017194ee859d79ec1d8ce
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-05-26 22:59:51 +0200

    You can find moyai statues now

--- src/main/java/com/ninni/yippee/block/YippeeBlocks.java
+++ src/main/java/com/ninni/yippee/block/YippeeBlocks.java
@@ -1,8 +1,11 @@
 package com.ninni.yippee.block;
 
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.loot.LootTables;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -13,7 +16,17 @@ public class YippeeBlocks {
 
     public static final Block WHOOPEE_CUSHION = register("whoopee_cushion", new WhoopeeCushionBlock(AbstractBlock.Settings.of(Material.AGGREGATE).requiresTool().strength(0.4F).sounds(BlockSoundGroup.WOOL)));
 
+
     private static Block register(String id, Block block) {
         return Registry.register(Registry.BLOCK, new Identifier(MOD_ID, id), block);
+    }
+
+    static {
+        LootTableLoadingCallback.EVENT.register((resourceManager, manager, id, supplier, setter) -> {
+            if (id.equals(LootTables.JUNGLE_TEMPLE_CHEST)) { supplier.copyFrom(manager.getTable(createDefaultLootTable(LootTables.JUNGLE_TEMPLE_CHEST))); }
+        });
+    }
+    private static Identifier createDefaultLootTable(Identifier base) {
+        return new Identifier(base.getNamespace(), "%s/%s".formatted(MOD_ID, base.getPath()));
     }
 }
