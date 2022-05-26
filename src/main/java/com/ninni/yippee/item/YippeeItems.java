6aa11a24d923da1c2514b67f95b302c150ae4bf6
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-05-26 22:11:50 +0200

    rarity changes

--- src/main/java/com/ninni/yippee/item/YippeeItems.java
+++ src/main/java/com/ninni/yippee/item/YippeeItems.java
@@ -15,8 +15,8 @@ import static com.ninni.yippee.Yippee.*;
 public class YippeeItems {
 
     public static final Item CREATURE = register("creature", new CreatureItem(new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(1).rarity(Rarity.UNCOMMON)));
-    public static final Item MOYAI_STATUE = register("moyai_statue", new MoyaiStatueItem(new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(1).rarity(Rarity.UNCOMMON)));
-    public static final Item WHOOPEE_CUSHION = register("whoopee_cushion", new BlockItem(YippeeBlocks.WHOOPEE_CUSHION, new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(16).rarity(Rarity.UNCOMMON)));
+    public static final Item MOYAI_STATUE = register("moyai_statue", new MoyaiStatueItem(new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(1)));
+    public static final Item WHOOPEE_CUSHION = register("whoopee_cushion", new BlockItem(YippeeBlocks.WHOOPEE_CUSHION, new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(16)));
 
     private static Item register(String id, Item item) { return Registry.register(Registry.ITEM, new Identifier(MOD_ID, id), item); }
 }
