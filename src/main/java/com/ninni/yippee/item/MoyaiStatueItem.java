5a8fe906040fe00d3d7be4abb2d641eadb8a933a
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-05-26 21:41:11 +0200

    Moyai improvements

--- src/main/java/com/ninni/yippee/item/MoyaiStatueItem.java
+++ src/main/java/com/ninni/yippee/item/MoyaiStatueItem.java
@@ -4,15 +4,11 @@ import com.ninni.yippee.sound.YippeeSoundEvents;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-
-import java.util.Random;
 
 public class MoyaiStatueItem extends Item {
 
@@ -21,7 +17,7 @@ public class MoyaiStatueItem extends Item {
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if (world.isClient) {
-            world.playSound(user, user.getBlockPos(), YippeeSoundEvents.ITEM_MOYAI_BOOM, SoundCategory.MASTER, 1.5F, 1.0F);
+            world.playSound(user, user.getBlockPos(), YippeeSoundEvents.ITEM_MOYAI_BOOM, SoundCategory.MASTER, 0.75F, 1.0F);
             user.getItemCooldownManager().set(this, 15);
             user.incrementStat(Stats.USED.getOrCreateStat(this));
             return TypedActionResult.success(user.getStackInHand(hand), world.isClient());
