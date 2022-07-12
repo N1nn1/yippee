adf3ed22c1a2a11e0ff57c64b70b89f3fcace57b
andantet <orrdaniel73@gmail.com>
2022-07-12 02:26:45 +0100

    fix yippee

--- src/main/java/com/ninni/yippee/item/TBHItem.java
+++ src/main/java/com/ninni/yippee/item/TBHItem.java
@@ -5,32 +5,24 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
-
 public class TBHItem extends Item {
-
     public TBHItem(Settings settings) { super(settings); }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        world.playSound(user, user.getBlockPos(), YippeeSoundEvents.ITEM_CREATURE_YIPPEE, SoundCategory.MASTER, 1.5F, 1.0F);
+        world.playSoundFromEntity(user, user, YippeeSoundEvents.ITEM_CREATURE_YIPPEE, SoundCategory.MASTER, 1.5F, 1.0F);
         user.getItemCooldownManager().set(this, 8);
         user.incrementStat(Stats.USED.getOrCreateStat(this));
         Vec3d vec3d = user.getBoundingBox().getCenter();
-        Random random = world.getRandom();
-        for (int i = 0; i < 100; ++i) {
-            double velX = random.nextGaussian() * 1.75;
-            double velY = random.nextGaussian() * 1.75;
-            double velZ = random.nextGaussian() * 1.75;
-            world.addParticle(ParticleTypes.ENTITY_EFFECT, vec3d.x, vec3d.y - 0.15, vec3d.z, velX, velY, velZ);
-        }
+        if (world instanceof ServerWorld serverWorld) serverWorld.spawnParticles(ParticleTypes.ENTITY_EFFECT, vec3d.x, vec3d.y - 0.15, vec3d.z, 100, 0, 0, 0, 1.75D);
         return TypedActionResult.success(user.getStackInHand(hand));
     }
 }
