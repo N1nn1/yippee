6c21f73ce50a5a847cbd55459c8ce0fe8c0719f3
0rc1nus <mitchellctlui@gmail.com>
2022-08-01 19:47:45 +0800

    Synchronized yet again

--- src/main/java/com/ninni/yippee/Yippee.java
+++ src/main/java/com/ninni/yippee/Yippee.java
@@ -7,6 +7,8 @@ import com.ninni.yippee.init.YippeeBlockEntityTypes;
 import com.ninni.yippee.init.YippeeSoundEvents;
 import com.ninni.yippee.init.YippeeItems;
 import com.ninni.yippee.network.YippeeNetworkHandler;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
@@ -16,6 +18,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(Yippee.MOD_ID)
 public class Yippee {
 	public static final String MOD_ID = "yippee";
+	public static final CreativeModeTab TAB = new CreativeModeTab("yippee") {
+		@Override
+		public ItemStack makeIcon() {
+			return new ItemStack(YippeeItems.YIPPEE.get());
+		}
+	};
 
 	public Yippee() {
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
--- src/main/java/com/ninni/yippee/init/YippeeItems.java
+++ src/main/java/com/ninni/yippee/init/YippeeItems.java
@@ -1,5 +1,6 @@
 package com.ninni.yippee.init;
 
+import com.ninni.yippee.Yippee;
 import com.ninni.yippee.item.MoyaiStatueItem;
 import com.ninni.yippee.item.TBHItem;
 import net.minecraft.world.item.BlockItem;
@@ -20,13 +21,14 @@ public class YippeeItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final RegistryObject<Item> TBH = ITEMS.register("tbh", () -> new TBHItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1)));
-    public static final RegistryObject<Item> MOYAI_STATUE = ITEMS.register("moyai_statue", () -> new MoyaiStatueItem(new Item.Properties().tab(CreativeModeTab.TAB_MISC).stacksTo(1).rarity(Rarity.UNCOMMON)));
-    public static final RegistryObject<Item> WHOOPEE_CUSHION = ITEMS.register("whoopee_cushion", () -> new BlockItem(YippeeBlocks.WHOOPEE_CUSHION.get(), new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE).stacksTo(16)));
-    public static final RegistryObject<Item> TACO_BELL = ITEMS.register("taco_bell", () -> new BlockItem(YippeeBlocks.TACO_BELL.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
-    public static final RegistryObject<Item> WEIGHT = ITEMS.register("weight", () -> new BlockItem(YippeeBlocks.WEIGHT.get(), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS)));
-    public static final RegistryObject<Item> DAMAGED_DISC = ITEMS.register("damaged_disc", () -> new RecordItem(15, YippeeSoundEvents.ITEM_RECORD_SCRATCH1, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE)));
-    public static final RegistryObject<Item> BROKEN_DISC = ITEMS.register("broken_disc", () -> new RecordItem(15, YippeeSoundEvents.ITEM_RECORD_SCRATCH2, new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> YIPPEE = ITEMS.register("yippee", () -> new Item(new Item.Properties().stacksTo(1).fireResistant().rarity(Rarity.EPIC)));
+    public static final RegistryObject<Item> TBH = ITEMS.register("tbh", () -> new TBHItem(new Item.Properties().tab(Yippee.TAB).stacksTo(1)));
+    public static final RegistryObject<Item> MOYAI_STATUE = ITEMS.register("moyai_statue", () -> new MoyaiStatueItem(new Item.Properties().tab(Yippee.TAB).stacksTo(1).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> WHOOPEE_CUSHION = ITEMS.register("whoopee_cushion", () -> new BlockItem(YippeeBlocks.WHOOPEE_CUSHION.get(), new Item.Properties().tab(Yippee.TAB).stacksTo(16)));
+    public static final RegistryObject<Item> TACO_BELL = ITEMS.register("taco_bell", () -> new BlockItem(YippeeBlocks.TACO_BELL.get(), new Item.Properties().tab(Yippee.TAB)));
+    public static final RegistryObject<Item> WEIGHT = ITEMS.register("weight", () -> new BlockItem(YippeeBlocks.WEIGHT.get(), new Item.Properties().tab(Yippee.TAB)));
+    public static final RegistryObject<Item> DAMAGED_DISC = ITEMS.register("damaged_disc", () -> new RecordItem(15, YippeeSoundEvents.ITEM_RECORD_SCRATCH1, new Item.Properties().stacksTo(1).tab(Yippee.TAB).rarity(Rarity.RARE)));
+    public static final RegistryObject<Item> BROKEN_DISC = ITEMS.register("broken_disc", () -> new RecordItem(15, YippeeSoundEvents.ITEM_RECORD_SCRATCH2, new Item.Properties().stacksTo(1).tab(Yippee.TAB).rarity(Rarity.RARE)));
 
 }
 
